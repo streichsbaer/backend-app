70a9009b71f77ce951e9aefa7ddf0b7806a71aa3
Stefan Streichsbier <stefan@streichsbier.at>
2019-07-11 15:09:24 +0800

    Update login.js

--- src/login.js
+++ src/login.js
@@ -4,4 +4,4 @@ require('./current_file').file(__filename);
 * Simple SQLi
 */
 
-// var query = 'SELECT * from user where id=' + req.body.login + ';';
+var query = 'SELECT * from user where id=' + req.body.login + ';';
