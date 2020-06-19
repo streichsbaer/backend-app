5a84f047c8c52c8e0b995645e25783a12fcaba23
Stefan Streichsbier <stefan@streichsbier.at>
2020-06-19 11:45:18 +0700

    Update login

--- src/login.js
+++ src/login.js
@@ -1,3 +1,3 @@
 require('./current_file').file(__filename);
 
-// var query = 'SELECT * from user where id=' + req.body.login + ';';
+var query = 'SELECT * from user where id=' + req.body.login + ';';
