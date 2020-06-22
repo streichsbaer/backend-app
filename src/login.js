c7cd40ca7fbd850f5edebfd90ae7c244331894dd
Stefan Streichsbier <stefan@streichsbier.at>
2020-06-22 20:12:59 +0700

    Fix insecure SQL query

--- src/login.js
+++ src/login.js
@@ -1,3 +1,12 @@
 require('./current_file').file(__filename);
 
 var query = 'SELECT * from user where id=' + req.body.id + ';';
+
+sequelize
+  .query("SELECT * FROM user WHERE id = :id ", {
+    raw: true,
+    replacements: { id: req.body.id }
+  })
+  .then(projects => {
+    // Do your stuff
+  });
