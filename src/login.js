6c618c4cfd5682e6c2ab2f72c0c56de3fa9d9c57
Stefan Streichsbier <stefan@streichsbier.at>
2020-10-19 09:23:59 +0700

    Fix Indentation

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 var db = require('../models');
 
 module.exports.userSearch = function (req, res) {
-  var query = "SELECT * from user where id=1";
+  var query = "SELECT * from user where id=2";
 	// var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
 	db.sequelize.query(query, {
 		model: db.User
