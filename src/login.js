2b39325f04a6f7b80b2b613f47caa5e4ffc6b57b
Stefan Streichsbier <stefan@streichsbier.at>
2020-10-19 09:18:15 +0700

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,5 +1,31 @@
-require("./current_file").file(__filename);
+var db = require('../models');
 
-// SQLi example
-// var query = "SELECT * from user where id=1;
-// var query = "SELECT * from user where id=" + req.body.login + ";";
+module.exports.userSearch = function (req, res) {
+  var query = "SELECT * from user where id=1";
+  //var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
+	db.sequelize.query(query, {
+		model: db.User
+	}).then(user => {
+		if (user.length) {
+			var output = {
+				user: {
+					name: user[0].name,
+					id: user[0].id
+				}
+			}
+			res.render('app/usersearch', {
+				output: output
+			})
+		} else {
+			req.flash('warning', 'User not found')
+			res.render('app/usersearch', {
+				output: null
+			})
+		}
+	}).catch(err => {
+		req.flash('danger', 'Internal Error')
+		res.render('app/usersearch', {
+			output: null
+		})
+	})
+}
