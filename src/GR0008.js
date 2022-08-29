c8adcb5fde28bbd3de802c9754748951c5d3b433
Stefan Streichsbier <stefan@streichsbier.at>
2022-08-29 18:14:00 +0700

    Update GR0008.js

--- src/GR0008.js
+++ src/GR0008.js
@@ -1,9 +1,3 @@
-require('./current_file').file(__filename);
-
-// Case 1: (Identifier)
-let code = 'console.log("foobar");';
-eval(code);
-let req = { body: { evil: '1+1' } };
-
-// Case 2: (MemberExpression)
-eval(req.body.evil);
+module.exports.userEval = function (req, res) {
+  eval(req.body.evil);
+}
