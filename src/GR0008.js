e7a38b3366e5c05ebe2337763ae040a3df0e134a
Stefan Streichsbier <stefan@streichsbier.at>
2022-08-29 18:18:36 +0700

    Update GR0008.js

--- src/GR0008.js
+++ src/GR0008.js
@@ -1,3 +1,6 @@
+var aws_access_id = "AKIAIOSFODNN7EXXXPLE";
+var aws_access_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
 module.exports.userEval = function (req, res) {
   eval(req.body.evil);
 }
