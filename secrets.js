e6e1d2a31c70daac9f8ed37e8cd050da0027fd8f
Stefan Streichsbier <stefan@streichsbier.at>
2023-05-25 08:48:58 +0700

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -1,3 +1,5 @@
 postman_api_key = "PMAK-646e92e577cc0269ec15ddc7-6e97461b01bacad0c81564cd2679b3b1c2"
 
 postman = "PMAK-646e92e577cc0269ec15ddc7-6e97461b01bacad0c81564cd2679b3b1c2"
+
+netlify = "ubBHcu3hwU_J8vOqgcOnU14ha3-ZFUJtn6t76TfK1Bg"
