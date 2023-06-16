ddb04a5392d0e0a9868a614aabf7f3d4d3248dc8
Stefan Streichsbier <stefan@guardrails.io>
2023-06-16 16:47:51 +0700

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,11 +1,9 @@
 var db = require('../models');
 
 module.exports.userSearch = function (req, res) {
-	var sqlq1 = "SELECT name,id FROM Users WHERE login= :login";
+	var sqlq1 = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
 	db.sequelize.query(sqlq1, {
-		model: db.User,
-		raw: true,
-  		replacements: { login: req.body.login }
+		model: db.User
 	}).then(user => {
 		if (user.length) {
 			var output = {
