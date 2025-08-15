891db7f5b8f340838adf207386b69bd7e71bb886
Stefan Streichsbier <stefan@guardrails.io>
2025-08-15 14:53:21 +0700

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,12 @@
+require('./current_file').file(__filename);
+
+/*
+* [GR:0004:alpha]
+* Disable markup escape detected: Handlebars#SafeString
+*/
+
+var Handlebars = require('handlebars');
+Handlebars.SafeString("<button>I agree I</button>")
+
+// This would trigger an issue
+Handlebars.SafeString("<button>aaaa</button>")
