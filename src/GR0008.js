24972b812a99efe27556012589c7c47566758273
Stefan Streichsbier <stefan@streichsbier.at>
2026-01-28 16:57:50 +0700

    remove the secret

--- src/GR0008.js
+++ src/GR0008.js
@@ -1,5 +1,3 @@
-var aws_access_id = "AKIAIOSFODNN7EXXXPLE";
-var aws_access_password = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXMPLEKEEY";
 
 module.exports.userEval = function (req, res) {
   eval(req.body.evil);
