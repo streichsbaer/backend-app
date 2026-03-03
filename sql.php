d1e4a080647e747e2b27a758fccb3d5ac486d398
Stefan Streichsbier <stefan@guardrails.io>
2026-03-03 12:26:47 +0700

    Add password update query for accounttable

--- sql.php
+++ sql.php
@@ -6,3 +6,4 @@ $query = "SELECT id, name, inserted, size FROM products
 WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
 $query = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid';";
+$query = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid';";
