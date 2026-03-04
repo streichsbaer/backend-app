2634c5a973437fa1f1a22c87103997bfb3392118
Stefan Streichsbier <stefan@guardrails.io>
2026-03-04 13:09:11 +0700

    Add pagination to accounttable update query
    Added a new query to update accounttable with pagination.

--- sql.php
+++ sql.php
@@ -6,4 +6,5 @@ $query = "SELECT id, name, inserted, size FROM products
 WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
 $query = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid';";
-$query = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid';";
+$yet_another_query = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid' ORDER BY name LIMIT 20 OFFSET $offset;";
+$result = odbc_exec($conn, $yet_another_query);
