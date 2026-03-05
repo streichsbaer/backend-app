c4e4a98d1c24e6ab6b6230decdbf05b89aac0d85
Stefan Streichsbier <stefan@streichsbier.at>
2026-03-05 10:51:22 +0700

    just adding some stuff

--- sql.php
+++ sql.php
@@ -7,3 +7,5 @@ WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
 $query = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid';";
 $query = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid';";
+$aqa = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid' ORDER BY name LIMIT 20 OFFSET $offset;";
+$result = odbc_exec($conn, $aqa);
