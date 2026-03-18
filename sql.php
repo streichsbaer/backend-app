1e23c03ea469cacc86c1f47044518b8b282c79e0
Stefan Streichsbier <stefan@guardrails.io>
2026-03-18 15:19:03 +0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,7 +1,7 @@
 <?php
 $offset = $_GET['offset']; // beware, no input validation!
-$query = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
-$result = pg_query($conn, $query);
+$queryy = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
+$result = pg_query($conn, $queryy);
 $query = "SELECT id, name, inserted, size FROM products
 WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
