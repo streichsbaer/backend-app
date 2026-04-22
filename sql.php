d939f7b18c7829a59bc3c440de85e0b517e4997e
Stefan Streichsbier <stefan@guardrails.io>
2026-04-22 16:17:29 +0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -6,5 +6,3 @@ $query = "SELECT id, name, inserted, size FROM products
 WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
 $query = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid';";
-$new_query_andrew = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid' LIMIT 20 OFFSET $offset;";
-$result = odbc_exec($conn, $new_query_andrew);
