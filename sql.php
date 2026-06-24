9025b88475a00aab37b9c8abed9482e88e65815e
Stefan Streichsbier <stefan@guardrails.io>
2026-06-24 19:22:00 +0700

    Modify password update query with LIMIT and OFFSET

--- sql.php
+++ sql.php
@@ -5,5 +5,4 @@ $result = pg_query($conn, $query);
 $query = "SELECT id, name, inserted, size FROM products
 WHERE size = '$offset'";
 $result = odbc_exec($conn, $query);
-$query = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid';";
-$query = "UPDATE accounttable SET pwd='$pwd' WHERE uid='$uid';";
+$query_test_grep = "UPDATE usertable SET pwd='$pwd' WHERE uid='$uid' LIMIT 20 OFFSET $offset;";
